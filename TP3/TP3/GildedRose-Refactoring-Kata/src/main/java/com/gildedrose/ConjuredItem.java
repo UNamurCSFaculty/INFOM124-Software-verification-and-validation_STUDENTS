3c33d23453894a467b58d979148ed320a56050a0
Denis Detry <73026883+denisdetry@users.noreply.github.com>
2023-11-22 16:04:55 +0100

    Add files via upload

---
+++ TP3/TP3/GildedRose-Refactoring-Kata/src/main/java/com/gildedrose/ConjuredItem.java
@@ -0,0 +1,24 @@
+package com.gildedrose;
+
+public class ConjuredItem extends Item{
+
+    public ConjuredItem(String name, int sellIn, int quality) {
+        super(name, sellIn, quality);
+    }
+
+    @Override
+    public void updateQuality() {
+        if (this.quality <= 0) return;
+
+        if (this.sellIn <= 0) {
+            this.quality -= 4;
+        } else {
+            this.quality -= 2;
+        }
+    }
+
+    @Override
+    public void updateSellIn() {
+        this.sellIn -= 1;
+    }
+}
