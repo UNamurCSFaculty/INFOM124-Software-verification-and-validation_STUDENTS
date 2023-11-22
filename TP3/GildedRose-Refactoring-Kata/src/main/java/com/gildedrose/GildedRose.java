228dc1d89316f7d5c3fb8c830d874c4afaa93dd7
NoHBruh <noe.remy@student.unamur.be>
2023-11-22 15:42:28 +0100

    Refactoring update_quality() GildelRose

--- TP3/GildedRose-Refactoring-Kata/src/main/java/com/gildedrose/GildedRose.java
+++ TP3/GildedRose-Refactoring-Kata/src/main/java/com/gildedrose/GildedRose.java
@@ -8,55 +8,45 @@ class GildedRose {
     }
 
     public void updateQuality() {
+        assert items != null : "items undefined";
+        if (items.length == 0){ return;}
+
         for (int i = 0; i < items.length; i++) {
-            if (!items[i].name.equals("Aged Brie")
-                    && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (items[i].quality > 0) {
-                    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                        items[i].quality = items[i].quality - 1;
-                    }
-                }
-            } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1;
+            items[i].updateSellIn();
 
-                    if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
+            switch (items[i].name) {
+                case "Sulfuras, Hand of Ragnaros":
+                    items[i] = new Sulfura(items[i].name, items[i].sellIn, items[i].quality);
+                    break;
 
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-                    }
-                }
+                case "Aged Brie":
+                    items[i] = new AgedBrie(items[i].name, items[i].sellIn, items[i].quality);
+                    break;
+
+                case "Backstage passes to a TAFKAL80ETC concert":
+                    items[i] = new Backstage(items[i].name, items[i].sellIn, items[i].quality);
+                    break;
+
+                case "Conjured":
+                    items[i] = new Conjured(items[i].name, items[i].sellIn, items[i].quality);
+                    break;
+                default:
+                    break;
             }
-
-            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                items[i].sellIn = items[i].sellIn - 1;
-            }
-
-            if (items[i].sellIn < 0) {
-                if (!items[i].name.equals("Aged Brie")) {
-                    if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].quality > 0) {
-                            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                                items[i].quality = items[i].quality - 1;
-                            }
-                        }
-                    } else {
-                        items[i].quality = items[i].quality - items[i].quality;
-                    }
-                } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1;
-                    }
-                }
-            }
+            items[i].updateQuality();
         }
     }
 }
+
+
+//cas de base -> qualité -1 puis -2 si sellin = 0
+// Sulfura -> on ne touche pas
+// Age Brie -> qualité +1 jusque 50
+// Backstage -> qualité +1 si sellin > 10 , +2 entre 10 et 6 et +3 entre 5 et 1 -- 0 quand sellin à 0
+// Conjured -> qualité -2, -4 quand sellin à 0
+
+//"Backstage passes to a TAFKAL80ETC concert"
+//""Sulfuras, Hand of Ragnaros""
+//"Aged Brie"
+//"Conjured"
+
